dbb63d9d3752efd5dbf13c0dc4a7718119a70435
Florian <floriangeorges1992@gmail.com>
2025-01-01 19:41:20 +0100

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      <h1>Cookchef</h1>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
