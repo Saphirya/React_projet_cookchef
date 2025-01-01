713e4eedf6bba2f2a4c990c73b83395f715c62c0
Florian <floriangeorges1992@gmail.com>
2025-01-01 20:49:47 +0100

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import styles from "./App.module.scss";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
-      <Content />
-      <Footer />
+      {/* <Content />
+      <Footer /> */}
     </div>
   );
 }
