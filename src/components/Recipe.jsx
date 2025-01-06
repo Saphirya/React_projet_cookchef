9acb07d8f1a2f39871afef30282324cf54a68389
Florian <floriangeorges1992@gmail.com>
2025-01-06 09:59:38 +0100

    add functionhandleClick for liked

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,8 +1,14 @@
 import styles from "./Recipe.module.scss";
+import { useState } from "react";
 
 function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
   return (
-    <div className={styles.recipe}>
+    <div onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
         <img src={image} alt="recipe" />
       </div>
@@ -10,7 +16,7 @@ function Recipe({ title, image }) {
         className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
         <h3 className="mb-10">{title}</h3>
-        <i className="fa-solid fa-heart"></i>
+        <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""} `}></i>
       </div>
     </div>
   );
